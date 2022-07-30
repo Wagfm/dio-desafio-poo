0f1abf8055f137ece2125af2bafacd738d351fa0
Wagfm <wagner.varginha@hotmail.com>
2022-07-30 00:28:39 -0300

    final

--- src/br/dio/desafio/domain/Main.java
+++ src/br/dio/desafio/domain/Main.java
@@ -1,18 +1,24 @@
 package br.dio.desafio.domain;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Bootcamp bootcamp = new Bootcamp("Bootcamp Java Developer", "Development in Java");
-        bootcamp.getContents().add(new Course("Course A", "Basics", 6));
         bootcamp.getContents().add(new Mentoring("Mentoring A", "Intro", LocalDate.now()));
+        bootcamp.getContents().add(new Course("Course A", "Basics I", 4));
+        bootcamp.getContents().add(new Course("Course B", "Basics II", 6));
         Dev dev = new Dev("Wagner");
         dev.enrollBootcamp(bootcamp);
-        dev.advance();
         System.out.println("Enrolled contents" + dev.getEnrolledContents());
-        System.out.println("Enrolled contents" + dev.getFinishedContents());
-
+        System.out.println("Finished contents" + dev.getFinishedContents());
+        dev.advance();
+        System.out.println("Advance...");
+        System.out.println("Enrolled contents" + dev.getEnrolledContents());
+        System.out.println("Finished contents" + dev.getFinishedContents());
+        dev.advance();
+        System.out.println("Advance...");
+        System.out.println("Enrolled contents" + dev.getEnrolledContents());
+        System.out.println("Finished contents" + dev.getFinishedContents());
     }
 }
